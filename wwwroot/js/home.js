dd9fc56ba275bba8934101f469d700f0d20ee13f
Affaz <affaz.a@hotmail.com>
2022-08-01 15:12:17 +0500

    client side, bootsrap, css, scripting

---
+++ wwwroot/js/home.js
@@ -0,0 +1,26 @@
+﻿// this is our js script file
+
+$(function () { // onload
+
+    // wait and then call
+    $('#noti-message').html("<strong>Please wait...</strong>");
+    $('#noti-alert').addClass('show');
+
+    setTimeout(fetchNotificaitons, 2500);
+});
+
+function fetchNotificaitons() {
+    // send request to server: checkNotificaitons
+    // receive notification
+    // update user with alert
+    var response = { "count": 33, "message": "You have 33 notifications" }; //RESP
+
+    // update the msesage inside alert
+    var retMessg = "You have <strong>" + response.count + " new notifications.</strong>";
+    $('#noti-message').html(retMessg);
+
+    //// show notification alert
+    //$('#noti-alert').addClass('show');
+
+    console.log('==>> notificaiton updated');
+}
